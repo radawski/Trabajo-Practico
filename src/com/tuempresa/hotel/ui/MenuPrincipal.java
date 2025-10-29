3543cdc6cac040235ca77187798336b4055f4422
radawski <agustinradawski18@gmail.com>
2025-10-29 20:06:01 -0300

    GestorPasajero para arquitectura por capas

--- src/com/tuempresa/hotel/ui/MenuPrincipal.java
+++ src/com/tuempresa/hotel/ui/MenuPrincipal.java
@@ -2,17 +2,21 @@ package com.tuempresa.hotel.ui;
 import java.util.Scanner;
 
 import com.tuempresa.hotel.dao.PasajeroDAO;
+import com.tuempresa.hotel.modelo.gestor.GestorPasajero;
 
 public class MenuPrincipal {
+
+    private static GestorPasajero gestorPasajero = new GestorPasajero(new PasajeroDAO("pasajeros.txt"));
+
     public static void mostrarMenuPrincipal(Scanner scanner){
         boolean salir = false;
         
         while (!salir) {
             System.out.println("\n*** MENÚ DE OPCIONES ***");
-            System.out.println("1. Buscar un husped");
-            System.out.println("2. Modificar un husped");
-            System.out.println("3. Dar de alta un husped");
-            System.out.println("4. Dar de baja un husped");
+            System.out.println("1. Buscar un huesped");
+            System.out.println("2. Modificar un huesped");
+            System.out.println("3. Dar de alta un huesped");
+            System.out.println("4. Dar de baja un huesped");
             System.out.println("5. Salir del programa");
             System.out.print("Ingrese su opción: ");
             
@@ -44,22 +48,22 @@ public class MenuPrincipal {
     
     private static void buscarHuesped(Scanner scanner) {
         System.out.println("\nEjecutando la función: BUSCAR HUESPED...");
-        PasajeroDAO dao = new PasajeroDAO("pasajeros.txt");
+
         System.out.println("\n BUSCAR: ");
 
         String datos = scanner.nextLine();
+        var resultados = gestorPasajero.buscarPasajerosPorDatos(datos);
 
         System.out.println("Resultados de la búsqueda para: '" + datos + "'");
-        if (dao.buscarPasajerosPorDatos(datos).isEmpty()) {
+
+        if (resultados.isEmpty()) {
             System.out.println("No se encontraron resultados.");
             //darDeAltaHuesped();
 
         } else {
             System.out.println("Pasajeros encontrados:");
-            dao.buscarPasajerosPorDatos(datos).forEach(p -> System.out.println(" - " + p.getNombre() + " " + p.getApellido() + ", " + p.getTipoDocumento().getTipo() + ": "+ p.getNroDocumento()));
+            resultados.forEach(p -> System.out.println(" - " + p.getNombre() + " " + p.getApellido() + ", " + p.getTipoDocumento().getTipo() + ": "+ p.getNroDocumento()));
         }
-        
-
-        
+         
     }
 }
